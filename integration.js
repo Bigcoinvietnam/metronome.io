85367897eb99810798009f44fd2e20623c6bc7f4
Felix Enriquez <felix@bloq.com>
2018-03-01 11:58:43 -0300

    add remove tag in the integration script, and better logs

--- integration.js
+++ integration.js
@@ -8,6 +8,12 @@ const CSS_DIR = path.join(STATIC_DIR, "css");
 
 const INDEX_HTML = "./index.html";
 
+// this is used to leave a mark for future removal in the next run of the script
+const INTEGRATION_ID = "auct-brd-integration";
+
+const ora = require('ora');
+const spinner = ora('starting process...').start();
+
 const getFileName = dir =>
   new Promise((resolve, reject) => {
     fs.readdir(dir, (err, files) => {
@@ -15,7 +21,7 @@ const getFileName = dir =>
 
       files.forEach(file => {
         if (path.extname(file) === ".map") return;
-        console.log("file founded: " + file);
+        spinner.succeed("file found: " + file);
         resolve(file);
       });
     });
@@ -30,6 +36,7 @@ const readFile = dir =>
   );
 
 const init = () => {
+  spinner.text = 'searching files needed for the integration...'
   const mainScript = getFileName(JS_DIR);
   const mainCSS = getFileName(CSS_DIR);
   const indexHTML = readFile(INDEX_HTML);
@@ -38,21 +45,36 @@ const init = () => {
 };
 
 init().then(([htmlFile, jsFile, cssFile]) => {
+  spinner.text = 'preparing html for change...'
   const $ = cheerio.load(htmlFile.toString());
-
-  const jsLink = `<script src="${path.join(JS_DIR, jsFile)}" />`;
-  $("body").append(jsLink);
+  
+  // removes the last tags with the old build number
+  spinner.text = 'removing old build tags...'
+  $(`head.link`, `#${INTEGRATION_ID}`).remove();
+  $(`body.script`, `#${INTEGRATION_ID}`).remove();
+  
+  // // creates new tags for the css and js
+  spinner.text = 'creating new tags from last build...'
+  const jsLink = `<script src="${path.join(
+    JS_DIR,
+    jsFile
+  )}" id="${INTEGRATION_ID}"/>`;
 
   const cssLink = `<link rel="stylesheet" href="${path.join(
     CSS_DIR,
     cssFile
-  )}" />`;
+  )}" id="${INTEGRATION_ID}" />`;
+
+  spinner.text = 'adding tags...'
+  $("body").append(jsLink);
   $("head").append(cssLink);
 
   const newIndex = $.html();
-  fs.writeFile("./newIndex.html", newIndex, function(err) {
-    if (err) {
-      return console.log(err);
-    }
+  
+  spinner.text = 'saving file...'
+  fs.writeFile("./index.html", newIndex, function(err) {
+    if (err) return spinner.fail(err)
+    spinner.succeed('index.html updated')
+    spinner.succeed('bye!')
   });
-});
+}).catch(err => spinner.fail(err));
